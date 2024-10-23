e1adfcd0d793be9fbad33610d07fde1d3b963bd9
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-23 19:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function validEmail(str) {
+
   //your JS code here.
+	function ValidEmail(str){
+             const regex= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,}$/;
+             return regex.test(str);
+        
 }
 
 // Do not change the code below.
